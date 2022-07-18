3e21cd259bbec86f03598071a7c098b3646e8aa2
Faiza <berrichif0717@gmail.com>
2022-07-18 14:16:08 +0200

    modif App.java

--- src/main/java/com/starterApp/App.java
+++ src/main/java/com/starterApp/App.java
@@ -1,16 +1,23 @@
 package com.starterApp;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.controller.MovieController;
 import com.repository.FileMovieRepository;
 import com.service.DefaultMovieService;
 
 public class App {
 	   public static void main( String[] args ) {
-	        MovieController movieController=new MovieController();
+	        /*MovieController movieController=new MovieController();
 	        FileMovieRepository movieRepository=new FileMovieRepository();
 	        DefaultMovieService movieService=new DefaultMovieService();
 	        movieController.setMovieService(movieService);
-	        movieService.setMovieRepository(movieRepository);
+	        movieService.setMovieRepository(movieRepository);*/
+		   ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+			MovieController movieController=context.getBean(MovieController.class);
+		   
+		   
 	        movieController.addUsingConsole();
 	    }
 	
